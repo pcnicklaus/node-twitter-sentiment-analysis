6a0d6db56a941cd63624a03b80548be336249ca0
Patrick <pcnicklaus@gmail.com>
2015-11-13 22:18:43 -0700

    did something or other and i have no clue what

--- twit-decision/config.js
+++ twit-decision/config.js
@@ -1,10 +1,10 @@
 var twitterKeys = {
-  consumer_key: 'gD0voDTz0CrFA97MeHjQw',
-  consumer_secret: 'GNmLK4KS2inmmg2dJSMVCqAG1PxxYHfo4cbvhxIGA',
-  access_token: '745911914-PHERXpcPRfNbNuXMP8r8zxtwEDqJJ3p4sXcoSlOv',
-  access_token_secret: 'WFFHDA9A1g6sY3ilM1yDeaxRMc9UcIDcVvVQqG1KLlc'
-}
+  consumer_key: 'tunpZqKR5uZkS5C3yAZyb4gTh',
+  consumer_secret: 'utGJcRZLNasgZEEseXbGbBVyggk6Dv0EVPh5zWsQUdQOwnsGcA',
+  access_token: '1636189436-3HtmLXcmg8nHgxVWU0PjjQHlACQtw7hj6mW8Uu8',
+  access_token_secret: 's8gIwDwVzWuuA1XYmoafV9Q0bzZUqnyG8u4SVkT740FK0'
+};
 
-module.exports = twitterKeys
+module.exports = twitterKeys;
 
 
